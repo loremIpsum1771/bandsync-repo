e78c1952ed021955aa0eb6d433172c4ceca7cfd0
Elijah Hawthorne <ceh451@nyu.edu>
2015-11-27 16:37:53 -0500

    single quotes added

--- bandsync/settings.py
+++ bandsync/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOSTS = 'smtp.gmail.com'
-EMAIL_HOSTS_USER = "bandsyncdjango@gmail.com"
-EMAIL_HOST_PASSWORD = "dmuy3193"
+EMAIL_HOSTS_USER = 'bandsyncdjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'dmuy3193'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
