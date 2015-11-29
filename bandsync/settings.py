8925a574361ed9049542f9cc6ff99dfa71d21adc
Elijah Hawthorne <ceh451@nyu.edu>
2015-11-28 19:07:53 -0500

    mysql settings added

--- bandsync/settings.py
+++ bandsync/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'bandsync.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ceyron',
+        'HOST' : "dm3193.com",
+        'PORT' : '3306',
+        'USER' : 'ceyron'
+        'PASSWD' : 'AB74BF43B5C1D9BEDF0BC'
     }
 }
 
