c44632ce60ded319ba6fc23a716decba0667b746
Elijah Hawthorne <ceh451@nyu.edu>
2015-12-20 00:13:34 -0500

    quote function added

--- artist_tracker/views.py
+++ artist_tracker/views.py
@@ -47,10 +47,10 @@ def home(request):
 def search(request):
 	form = SearchForm(request.POST or None)
 	if form.is_valid():
-		form_artistSelect = form.cleaned_data.get("artist_select")
-		form_city = form.cleaned_data.get("city")
-		form_state = form.cleaned_data.get("state")
-		mile_radius = form.cleaned_data.get("radius")
+		form_artistSelect = quote(form.cleaned_data.get("artist_select"))
+		form_city = quote(form.cleaned_data.get("city"))
+		form_state = quote(form.cleaned_data.get("state"))
+		mile_radius = quote(form.cleaned_data.get("radius"))
 		print "testing"
 		url = "http://api.bandsintown.com/events/search?artists[]=" + form_artistSelect + "&location=" +form_city+","+ form_state+"&radius="+ mile_radius + "&format=json&app_id=YOUR_APP_ID"
 		data = json.load(urllib2.urlopen(url))
