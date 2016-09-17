6442f3f41e5e5641f2ca2edfe1e8412c1290ad36
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2016-09-17 01:31:34 -0400

    Update admin.py

--- artist_tracker/admin.py
+++ artist_tracker/admin.py
@@ -8,7 +8,5 @@ from .models import SignUp
 class SignUpAdmin(admin.ModelAdmin):
 	list_display = ["__unicode__", "timestamp", "updated"]
 	form = SignUpForm
-	# class Meta:
-	# 	model = SignUp
 
 admin.site.register(SignUp, SignUpAdmin)
