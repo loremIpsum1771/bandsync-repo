03bf255cc68b34e84e6b15f29c3b5d93a92a93e4
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2016-09-17 01:34:02 -0400

    Update views.py

--- artist_tracker/views.py
+++ artist_tracker/views.py
@@ -13,8 +13,6 @@ from django.core.mail import send_mail
 
 def home(request):
 	title = "Welcome"
-	# if request.user.is_authenticated():
-	# 	title = "My Title %s" % (request.user)	
 
 	#add a form
 	form = SignUpForm(request.POST or None)
@@ -27,8 +25,6 @@ def home(request):
 
 	if form.is_valid():
 		form.save()
-		# instance = form.save(commit =False)
-
 		instance = form.save(commit = False)
 
 		full_name = form.cleaned_data.get("full_name")
@@ -49,71 +45,36 @@ def home(request):
 def search(request):
 	queryform = SearchForm(request.POST or None)
 	modalform = ModalForm(request.POST or None)
-	#print "query form is valid = " + str(modalform.is_valid())
 	if queryform.is_valid():
 		form_artistSelect = urllib2.quote(queryform.cleaned_data.get("artist_select"))
 		form_city =   urllib2.quote(queryform.cleaned_data.get("city"))
 		form_state = urllib2.quote(queryform.cleaned_data.get("state"))
 		mile_radius = urllib2.quote(queryform.cleaned_data.get("radius"))
-
-		#print "testing"
 		url = "http://api.bandsintown.com/artists/" + form_artistSelect + "/events/search.json?api_version=2.0&app_id=YOUR_APP_ID&location=" +form_city+","+ form_state+"&radius="+ mile_radius
 		data = json.load(urllib2.urlopen(url))
-
-		#url = "http://api.bandsintown.com/events/search?artists[]=" + form_artistSelect + "&location=" +form_city+","+ form_state+"&radius="+ mile_radius + "&format=json&app_id=YOUR_APP_ID"
 
 		context = {
 			"queryform" : queryform,
 			"modalform" : modalform,
 			"data": data
 		}
-		#titles = [ i.get("title") for i in data]
 		titles = json.dumps([i.get("title") for i in data])
-		
-		#formatted_dts = [i.get("formatted_datetime") for i in data]
 		ticket_urls = json.dumps([i.get("ticket_url").encode("utf-8") for i in data])
 		ticket_statuses = json.dumps([i.get("ticket_status").encode("utf-8") for i in data])
 		venues = json.dumps([i["venue"] for i in data])
 
-		# venue_names = [i.get("venue").get("name").encode("utf-8") for i in data]
-		# venue_cities = [i.get("venue").get("city").encode("utf-8") for i in data]
-		# venue_region = [i.get("venue").get("region").encode("utf-8") for i in data]
-
-		
-
-		# print "\ntitles\n"
-		# print titles
 		print "\ntitles\n"
 		print titles
 		print "\nformatted_dts\n"
-		# print formatted_dts
-		# print "\nticket_urls\n"
 		print ticket_urls
 		print "\nticket_statuses\n"
 		print ticket_statuses
-		# print "\nvenue names\n"
-		# print venue_names
-		# print "\nvenue cities\n"
-		# print venue_cities
-		# print "\nvenue region\n"
-		# print venue_region
 	
-		# context = {
-		# 	"queryform" : queryform,
-		# 	"modalform" : modalform,
-		# 	"form_artistSelect" : form_artistSelect,
-		# 	"raw_dts" : raw_dts,
-		# 	"ticket_urls" : ticket_urls,
-		# 	"ticket_statuses" : ticket_statuses,
-		# 	"venues" : venues,
-		# }
-
 	else:
 		context = {
 			"queryform" : queryform	
 
 		}
-	#print "modal form is valid = " + str(modalform.is_valid())
 	if modalform.is_valid():
 		form_recipient = modalform.cleaned_data.get("rec_email")
 	 	form_message = modalform.cleaned_data.get("message")
@@ -137,74 +98,14 @@ def search(request):
 	return render(request,"searchform.html" , context)
 
 
-# def invite(request, invite_name_url):
-# 	context = RequestContext(request)
-# 	invite_data = get_invite_Info()
-
-# 	context_dict = {'invite_data': invite_data }
-
-# 	try:
-
-# 	return render(request, "searchform.html")
-
-# @login_required
-# def invite_sender(request):
-# 	context = RequestContext(request)
-# 	concertDate = None
-# 	recipient = None
-# 	concert_id = None
-# 	artist_id = None
-# 	message = None
-# 	rec_name = None
-# 	ticket_url = None
-# 	if request.method == 'GET':
-# 		concertDate = request.GET['raw_dts']
-# 		recipient = request.GET['rec_email']
-# 		concert_id = request.GET['concert_id']
-# 		artist_id = request.GET['artist_id']
-# 		message = request.GET['message']
-# 		rec_name = request.GET['rec_name']
-# 		ticket_url = request.GET['ticket_url']
-
-# 	rec_email = recipient
-# 	message = message
-# 	rec_full_name = rec_name
-# 	concert_url = ticket_url
-
-	
-# 	subject = "Site contact form"
-# 	from_email = settings.EMAIL_HOST_USER
-# 	to_email = [rec_email, from_email, 'celijahh@gmail.com']
-# 	contact_message = ''' You have received a Concert Invite
-# 	%s: %s concert tickets url: %s via %s
-# 	'''%(rec_full_name, message,concert_url ,rec_email)
-# 	send_mail(subject, contact_message, from_email, to_email, fail_silently =False)
-
-# 	if request.user.is_authenticated():
-# 		user_id = request.user.id
-# 	if user_id:
-# 		user = User.objects.get(id=int(user_id))
-# 		concert = Concert.objects.get(id = int(concert_id))
-# 		artist = Artist.objects.get(id = int(artist_id))
-# 		if user:
-# 			invite = Invite(user, rec_email, concert, artist, message )
-# 			invite.save()
-# 	return render(request,"searchform.html")
-
-# def save(request):
-
-# 	return render(request, "searchform.html")
 
 
 def contact(request):
 	form = ContactForm(request.POST or None)
 	if form.is_valid():
-		# for key, value in form.cleaned_data.iteritems():
-		# 	print key, value
 		form_email = form.cleaned_data.get("email")
 		form_message = form.cleaned_data.get("message")
 		form_full_name = form.cleaned_data.get("full_name")
-		# print form.cleaned_data
 		subject = "Site contact form"
 		from_email = settings.EMAIL_HOST_USER
 		to_email = [from_email, 'celijahh@gmail.com']
@@ -219,16 +120,4 @@ def contact(request):
 	return render(request, "forms.html", context)
 
 def profile(request):
-	# user_concerts = Concert.objects.filter(user = request.user)
-	# print userconcerts
-	# sent_invites = Invite.objects.filter(user = request.user)
-	# print sentinvites
-	# followed_artists = Follow.objects.filter(user = request.user)
-	# print followed_artists
-
-	# context = {
-	# 	"user_concerts": user_concerts,
-	# 	"sent_invites" : sent_invites,
-	# 	"followed_artists" : followed_artists
-	# }
 	return render(request, "profilepage.html")#, context)
