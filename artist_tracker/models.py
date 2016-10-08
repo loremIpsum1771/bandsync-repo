7f991d0b6bc017f8ecb0c93c86aa3e505629e76f
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2016-10-07 20:14:38 -0400

    Update models.py

--- artist_tracker/models.py
+++ artist_tracker/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
-# Create your models here.
 class SignUp(models.Model):
 	email = models.EmailField()
 	full_name = models.CharField(max_length = 120, blank = True, null = True)
